6d8619f3f7dfba4b6a416985b8bce82c2fc663a7
jvpatel12 <148254662+jvpatel12@users.noreply.github.com>
2024-04-03 10:16:38 -0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main(String ags[]){
+        byte b=8;
+        System.out.println(b);
+        char ch='a';
+        System.out.println(ch);
+        boolean var=true;
+        System.out.println(var);
+        int numbers= 25;
+        System.out.println(numbers);
+        short n = 240;
+        System.out.println(n);
+    }
+}
